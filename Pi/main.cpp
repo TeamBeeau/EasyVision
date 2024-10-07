60df7a0e8ea45d74bae16f5ab1c90808252db2a1
TeamBeeau <teamrd@beeau.vn>
2024-10-07 11:42:02 +0700

    ...

---
+++ Pi/main.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+using namespace cv;
+int main(int argc, char** argv)
+{
+    cv::Mat raw = cv::imread("1.png");
+    // cv::imshow("aa", raw);
+     //g_print("Hello World\n");
+
+     return 1;
+}
---
+++ Pi/main.h
@@ -0,0 +1,7 @@
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui_c.h>
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/imgproc/types_c.h>
+
+
